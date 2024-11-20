28db413e4a04dea84c05579e65f3ed189e49c94d
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-11-20 21:49:09 +0530

    Add changes into EditorDemo

---
+++ EditorDemo/EditorDemo/Editor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorDemo
+{
+    public class Editor
+    {
+        public string content { get; set; }
+
+        public EditorState CreateState()
+        {
+            return new EditorState(content);
+        }
+
+        public void restore(EditorState state)
+        {
+            content = state.Content;
+        }
+    }
+}
---
+++ EditorDemo/EditorDemo/EditorState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorDemo
+{
+    public class EditorState
+    {
+        public string Content { get; }
+
+        public EditorState(string content)
+        {
+            this.Content = content;
+        }
+    }
+}
